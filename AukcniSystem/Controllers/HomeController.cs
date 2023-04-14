d41a58241e95d357875789e76f7cbba6a2a56964
Hynek Fišera <hynek.fisera@seznam.cz>
2023-04-14 17:26:39 +0200

    Allow balance change

--- AukcniSystem/Controllers/HomeController.cs
+++ AukcniSystem/Controllers/HomeController.cs
@@ -6,38 +6,50 @@ using System.Diagnostics;
 
 namespace AukcniSystem.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-        private readonly ApplicationDbContext _context;
-        private readonly UserManager<Klient> _userManager;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private readonly ApplicationDbContext _context;
+		private readonly UserManager<Klient> _userManager;
 
-        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context, UserManager<Klient> userManager)
-        {
-            _logger = logger;
-            _context = context;
-            _userManager = userManager;
-        }
+		public HomeController(ILogger<HomeController> logger, ApplicationDbContext context, UserManager<Klient> userManager)
+		{
+			_logger = logger;
+			_context = context;
+			_userManager = userManager;
+		}
 
-        public IActionResult Index()
-        {
-            return View(_context.Kategorie.ToList());
-        }
+		public IActionResult Index()
+		{
+			return View(_context.Kategorie.ToList());
+		}
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+		public IActionResult Privacy()
+		{
+			return View();
+		}
 
-        public IActionResult Prehled()
-        {
-            return View((_context.Aukce.Where(x => x.AutorId == _userManager.GetUserId(User)).ToList(), _context.Klienti.Where(x => x.Id == _userManager.GetUserId(User)).Select(x => x.Zustatek).SingleOrDefault()));
-        }
+		public IActionResult Prehled()
+		{
+			return View((_context.Klienti.Where(x => x.Id == _userManager.GetUserId(User)).Select(x => x.Zustatek).SingleOrDefault(), _context.Aukce.Where(x => x.AutorId == _userManager.GetUserId(User)).ToList(), _context.Prihozy.Where(x => x.KlientId == _userManager.GetUserId(User)).ToList()));
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		[HttpPost]
+		public IActionResult Prehled([FromForm] int novyZustatek)
+		{
+			var user = _context.Klienti.Where(x => x.Id == _userManager.GetUserId(User)).SingleOrDefault();
+			if (user != null)
+			{
+				user.Zustatek = novyZustatek;
+				_context.SaveChanges();
+			}
+			return View((_context.Klienti.Where(x => x.Id == _userManager.GetUserId(User)).Select(x => x.Zustatek).SingleOrDefault(), _context.Aukce.Where(x => x.AutorId == _userManager.GetUserId(User)).ToList(), _context.Prihozy.Where(x => x.KlientId == _userManager.GetUserId(User)).ToList()));
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
